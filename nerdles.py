4c9897bf3dfb44f7baa89b995f72420f9d32732f
Vyom Devgan <80549269+vyom-devgan@users.noreply.github.com>
2023-08-18 10:24:00 -0400

    added comments

--- nerdles.py
+++ nerdles.py
@@ -1,9 +1,17 @@
-#Vyom Devgan
+# Nerdle Game
+# Vyom Devgan
 
 import random
 import re
 
 def is_prime(n: int) -> bool:
+    '''
+    Checks if given integer (n) is prime.
+    
+    n (int): The number that is given which is checked.
+    
+    Returns a bool, indicating True if prime, False otherwise.
+    '''
     if n <= 1:
         return False
     for i in range(2, int(n ** 0.5) + 1):
@@ -12,16 +20,46 @@ def is_prime(n: int) -> bool:
     return True
 
 def final_solution() -> int:
+    '''
+    Generates the final solution for the Nerdle.
+    
+    A random integer between 1 and 999 is chosen at random.
+    That random integer is then checked for primality.
+    If the random integer is prime, a new random integer is selected recursively until number is not prime.
+    
+    Returns a non prime integer between 1 and 999.
+    '''
     solution_val = random.randint(1,999)
     if is_prime(solution_val):
         return final_solution
     return solution_val
 
 def operator() -> str:
-        operators = ['+','-','*','/'] # ['ADDITION', 'SUBTRACTION', 'DIVISION', 'MULTIPLICATION']
-        return operators[random.randint(0,3)]
+    '''
+    Generates a random arithmetic operator.
+    
+    A random basic arithmetic operator is selected: addition (+), subtraction (-), multiplication (*), or division (/).
+    
+    Returns a string representing an arithmetic operator.
+    '''
+    operators = ['+','-','*','/'] # ['ADDITION', 'SUBTRACTION', 'DIVISION', 'MULTIPLICATION']
+    return operators[random.randint(0,3)]
     
 def find_solutions(number: int, operation: str) -> list:
+    '''
+    Generates a list of solutions given a non prime integer and an arithmetic operator.
+    
+    number (int): The target integer for which solutions need to be found.
+    operation (str): The arithmetic operation to use ('+', '-', '*', or '/').
+    
+    Raises ValueError if an unsupported operation is provided.
+    For the division operation, solutions are generated as numerator / denominator = number.
+
+    Returns a list of strings representing solutions in the format "operand operator operand = number".
+
+    Example:
+        find_solutions(5, '+') could return ['0 + 5 = 5', '1 + 4 = 5', '2 + 3 = 5'].
+    '''
     solutions = []
     if operation == '+':
         for i in range(number + 1):
@@ -51,6 +89,20 @@ def find_solutions(number: int, operation: str) -> list:
     return solutions
 
 def solution_finder(final_answer: int, final_operator: str) -> tuple:
+    '''
+    Finds a solution for a given answer and operator, or regenerate new values and find solutions if an error occurs.
+    
+    final_answer (int): The target answer for which a solution needs to be found.
+    final_operator (str): The arithmetic operator to use ('+', '-', '*', or '/').
+    
+    Finds a solution for the given final answer and operator using the find_solutions function.
+    If the initial attempt fails, new values are generated to ensure a solution can be found.
+    
+    Returns a tuple that contains the final answer, final operator, and a randomly selected solution string.
+
+    Example:
+        solution_finder(10, '+') could return (15, '+', '5 + 10 = 15').
+    '''
     try:
         solutions = find_solutions(final_answer, final_operator)
         solve = solutions[random.randint(0,len(solutions)-1)]
@@ -62,6 +114,20 @@ def solution_finder(final_answer: int, final_operator: str) -> tuple:
     return final_answer, final_operator, solve
 
 def eval_check(correct_ans: str, guessed_ans: str) -> bool:
+    '''
+    Checks if a guessed arithmetic expression evaluates to the correct answer.
+    
+    correct_ans (str): The correct arithmetic expression in the format "operand operator operand = result".
+    guessed_ans (str): The guessed arithmetic expression in the same format as correct_ans.
+    
+    This function takes a  and a guessed expression as input.
+    It evaluates both the correct arithmetic and guessed arithmetic expressions and compares the results to determine if the guessed expression is correct.
+    
+    Returns a bool, indicating True if the guessed expression evaluates to the correct answer, False otherwise.
+
+    Example:
+        eval_check('2 + 3 = 5', '1 + 4 = 5') could return True.
+    '''
     solved = correct_ans.split('=')[0]
     guessed = guessed_ans.split('=')[0]
     if eval(solved) == eval(guessed):
@@ -69,6 +135,18 @@ def eval_check(correct_ans: str, guessed_ans: str) -> bool:
     return False
 
 def solution_checker(correct_ans: str, guessed_ans: str) -> None:
+    '''
+    Checks guessed arithmetic expression against the correct answer.
+    
+    correct_ans (str): The correct arithmetic expression in the format "operand operator operand = result".
+    guessed_ans (str): The guessed arithmetic expression in the same format as correct_ans.
+    
+    Identifies correct and incorrect elements by comparing the guessed expression against the correct.
+    It also tracks and updates green_list and red_list, which provide information to the player about which integers are in the solution.
+
+    Example:
+        solution_checker('2 + 3 = 5', '3 + 1 = 4') would provide feedback about correct and incorrect elements.
+    '''
     if eval_check(correct_ans, guessed_ans):
         return 'YOU WIN'
     
@@ -86,6 +164,18 @@ def solution_checker(correct_ans: str, guessed_ans: str) -> None:
     return
 
 def hinter(hints: int) -> str:
+    '''
+    Provides hints depending on the amount of hints remaining.
+
+    hints (int): The remaining hints count for the player.
+
+    The hints help players get closer to finding the solution to the arithmetic expression.
+
+    Returns a string with a hint message based on the provided hints count.
+
+    Example:
+        hinter(1) could return "The operation used in the expression is +."
+    '''
     if hints == 0:
         return f'\nThe final answer is of length: {str(len(str(final_answer)))}.\nMeaning there are {str(len(str(final_answer)))} digits in the solution to the expression.'
     elif hints == 1:
@@ -94,7 +184,24 @@ def hinter(hints: int) -> str:
         return f'\nThe whole solution has this many characters: {str(len(solve))} No Spaces'
     return 'You are out of Hints! Sorry!'
 
-def game():
+def game() -> str:
+    '''
+    Play Vyom's Math Game.
+    
+    Main game loop for Vyom's Math Game. Players need to guess a valid mathematical expression
+    that evaluates to the correct answer. They have a limited number of guesses and can use hints to aid their guesses.
+    
+    Players need to follow rules and hints to come up with a valid mathematical expression.
+    
+    Hints provide information about correct and incorrect elements in the solution.
+    
+    Players are allowed a maximum of 3 hints.
+    
+    Returns a string, message indicating whether the player won or lost the game.
+
+    Example:
+        game() could return "You Win!" if the player guesses the correct expression.
+    '''
     print('Welcome to Vyom\'s Math Game.'
           '\nIn this game you will have to come up with a mathematical expression that is true.'
           '\nYou will be given 5 tries to guess the whole soltuion.\n'
@@ -129,14 +236,19 @@ def game():
     return "\nYou LOST\n"
 
 if __name__ == "__main__":
+    # Initialize game parameters
     final_answer = final_solution()
     final_operator = operator()
     green_list = []
     red_list = []
+    
+    # Generate solution
     final_answer, final_operator, solve = solution_finder(final_answer, final_operator)
     solve = solve.replace(' ','')
     if '--' in solve:
         solve.replace('--', '+')
+        
+    # Play the game and display results
     print(game())
     print(f'The correct solution was: {solve}.\n')
     input('Thanks for playing!. Press any key to exit.')
